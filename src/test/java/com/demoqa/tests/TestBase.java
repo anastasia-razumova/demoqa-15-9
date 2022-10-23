098d058bcaac91c328a6de10f82aa847a8ca8258
anastasia.razumova <ra@affise.com>
2022-10-23 22:29:28 +0200

    add test

--- src/test/java/com/demoqa/tests/TestBase.java
+++ src/test/java/com/demoqa/tests/TestBase.java
@@ -22,7 +22,6 @@ public class TestBase {
 
         Configuration.browserCapabilities = capabilities;
         Configuration.baseUrl = "https://demoqa.com";
-        Configuration.holdBrowserOpen = true;
         Configuration.browserSize = "1920x1080";
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
     }
