0dfda44b7fd2d6fb70e7c3cc420d7dae18582c5b
anastasia.razumova <ra@affise.com>
2022-11-16 17:52:21 +0100

    add selenidetest

---
+++ src/test/java/com/demoqa/tests/SelenideTest.java
@@ -0,0 +1,28 @@
+package com.demoqa.tests;
+
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class SelenideTest {
+
+    @BeforeAll
+    static void setUp(){
+        Configuration.holdBrowserOpen = true;
+
+    }
+
+    @Test
+    public void checkPageLabel(){
+        open("https://github.com/");
+        $(byText("Solutions")).hover();
+        $(byText("Enterprise")).click();
+        $("h1").shouldHave(text("Build like the best"));
+    }
+}
